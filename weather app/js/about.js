7f8ddd7b4ba0e93b70ff6a2b6980b7d693de34f4
Yousef Khalid <alshaikhliyousuf7777@gmail.com>
2022-03-24 21:00:47 +0300

    4

--- weather app/js/about.js
+++ weather app/js/about.js
@@ -1,5 +1,3 @@
-// hamburger menu toggle.
-
 const toggleOn = document.querySelector(".toggle-on-cont");
 const active = document.querySelector(".drop-down-menu");
 const toggleOff = document.querySelector(".toggle-off-cont");
@@ -11,18 +9,3 @@ toggleOn.addEventListener("click", function () {
 toggleOff.addEventListener("click", function() {
 	active.classList.toggle("press")
 });
-
-// end of hamburger menu toggle.
-
-// automatic image slider 
-
-let counter = 1;
-setInterval(function() {
-  document.getElementById('radio' + counter).checked = true;
-	counter++;
-	if(counter > 7){
-		counter = 1;
-	}
-}, 5000);
-
-// end of automatic image slider
